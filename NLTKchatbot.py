140dffe19103302a5c984409da168e2bb1502d0e
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-11-18 12:52:42 -0500

    Update NLTKchatbot.py

--- NLTKchatbot.py
+++ NLTKchatbot.py
@@ -1,3 +1,11 @@
+# install necessary NLTK data:
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('maxent_ne_chunker')
+# nltk.download('words')
+
+
+
 import re
 import nltk
 from nltk.tokenize import word_tokenize
